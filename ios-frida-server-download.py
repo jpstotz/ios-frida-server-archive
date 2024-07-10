ce8bee1d2d27bad2e05f75282d053943292f56b7
Jan Peter Stotz <jan-peter.stotz@sit.fraunhofer.de>
2024-07-10 09:24:43 +0200

    save iphoneos-arm64 and iphoneos-arm version of frida server

--- ios-frida-server-download.py
+++ ios-frida-server-download.py
@@ -10,16 +10,22 @@ deb_list = cydiarepor.get_debs_from_cydiarepoURL(repo_url)
 
 for deb in deb_list:
     file_name = deb['Filename']
-    if 'iphoneos-arm64' in file_name:
+    print(file_name)
+    if 'iphoneos-arm' not in file_name:
+        continue
+    file = os.path.basename(file_name)
+    subdir = Path(file).stem.rpartition('_')[2]
+    if not os.path.exists(subdir):
+        os.mkdir(subdir)
+    file = os.path.join(subdir, file)
+    if os.path.exists(file):
+        print("{} already exists".format(file))
+        continue
 
-        file = os.path.basename(file_name)
-        if os.path.exists(file):
-            print("{} already exists".format(file))
-            exit(0)
+    print("Saving {}".format(file))
+    url = repo_url + '/' + file_name
+    urllib.request.urlretrieve(url, file)
 
-        url = repo_url + '/' + file_name
-        urllib.request.urlretrieve(url, file)
-
-        print(deb)
+    print(deb)
 
 exit(0)
